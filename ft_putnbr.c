80ca6e90609cd3154ab7b30a996bdb61e4682d2d
Ayhan Agdemir <aagdemir@2-F-1.42heilbronn.de>
2024-04-28 16:38:29 +0200

    strict fixes

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,22 +6,68 @@
 /*   By: aagdemir <aagdemir@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 18:50:14 by aagdemir          #+#    #+#             */
-/*   Updated: 2024/04/20 19:03:31 by aagdemir         ###   ########.fr       */
+/*   Updated: 2024/04/28 16:38:15 by aagdemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr(int d)
+int	ft_digitcounter(int n)
 {
-	char	*s;
-	int		len;
+	int	temp;
+	int	digit;
 
-	if (d == 0)
-		return (ft_putchar('0'));
-	s = ft_itoa(d);
-	len = ft_strlen(s);
-	ft_putstr(s);
-	free(s);
-	return (len);
+	digit = 0;
+	temp = n;
+	if (n == INT_MIN)
+		return (11);
+	if (n < 0)
+	{
+		temp = -temp;
+		digit++;
+	}
+	while (temp != 0)
+	{
+		digit++;
+		temp /= 10;
+	}
+	if (n == 0)
+		digit = 1;
+	return (digit);
+}
+
+int	ft_putnbr(int n)
+{
+	int	digitcounter;
+	int	check;
+
+	digitcounter = ft_digitcounter(n);
+	if (n == INT_MIN)
+	{
+		if(ft_putstr("-2147483648") == -1)
+			return -1;
+		return digitcounter;
+	}
+	if (n < 0)
+	{
+		check = ft_putchar('-');
+		if (check == -1)
+			return (-1);
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		if(ft_putnbr(n / 10) == -1)
+			return -1;
+		check = ft_putchar(n % 10 + '0');
+		if (check == -1)
+			return (-1);
+	}
+	else
+	{
+		check = ft_putchar(n + '0');
+		if (check == -1)
+			return (-1);
+	}
+	return (digitcounter);
 }
