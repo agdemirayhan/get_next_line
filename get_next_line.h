49bd31bea97b406aa8c617633e2451001a1e0075
Ayhan Agdemir <aagdemir@2-F-1.42heilbronn.de>
2024-05-09 14:22:19 +0200

    test

--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,9 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER SIZE 10
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER SIZE 10
+# endif
 
 # include <fcntl.h>
 # include <limits.h>
@@ -13,20 +13,19 @@
 # include <string.h>
 # include <unistd.h>
 
-struct node {
-	int value;
-	struct node *next;
-}
-typedef struct node node_t;
+struct				node
+{
+	int				value;
+	struct node		*next;
+};
+typedef struct node	node_t;
 
-
-
-char	*get_next_line(int fd);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
+char				*get_next_line(int fd);
+void				*ft_memcpy(void *dst, const void *src, size_t n);
+size_t				ft_strlen(const char *s);
+char				*ft_strchr(const char *s, int c);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
+char				*ft_strdup(const char *s1);
+char				*ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- structpractice.c
+++ structpractice.c
@@ -1,47 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct node {
-	int value;
-	struct node *next;
-};
-typedef struct node node_t;
-
-void printlist(node_t *head)
+struct				node
 {
-	node_t *temphead = head;
+	int				value;
+	struct node		*next;
+};
+typedef struct node	node_t;
 
+void	printlist(node_t *head)
+{
+	node_t	*temphead;
+
+	temphead = head;
 	while (temphead)
 	{
-		printf("%d - ",temphead->value);
-		temphead = temphead ->next;
+		printf("%d - ", temphead->value);
+		temphead = temphead->next;
 	}
 	printf("\n");
 }
 
-int main(void)
+node_t *create_new_node(int value)
 {
+	node_t *result = malloc(sizeof(node_t));
+	result->value = value;
+	result->next = NULL;
 
-	node_t n1, n2, n3;
+	return result;
+}
+
+int	main(void)
+{
+	node_t *n1, n2, n3;
 	node_t *head;
 
-	n1.value = 1;
+	n1 = malloc(sizeof(node_t));
+	n1->value = 1;
 	n2.value = 2;
 	n3.value = 3;
 
 	// link the list
-	head = &n3;
-	n3.next = &n2;
-	n2.next = &n1;
-	n1.next = NULL;
-
-	node_t n4;
-	n4.value = 4;
-
-	head = &n4;
-	n4.next = &n3;
-
+	head = n1;
+	n3.next = NULL;
+	n2.next = &n3;
+	n1->next = &n2;
 
 	printlist(head);
 
-	return 0;
+	return (0);
 }
